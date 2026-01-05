9e2fb5b8dceb74f432b3971bb881a503190a69d8
IceLeach <1547860187@qq.com>
2026-01-05 17:37:51 +0800

    refactor: update group usage in r3f

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
           'transparent',
           'map',
           'aoMap',
+          'geometry',
         ],
       },
     ],
--- src/pages/Cloudscape/r3f.tsx
+++ src/pages/Cloudscape/r3f.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { BufferGeometry, Color, EllipseCurve, Group, Line as ThreeLine, LineBasicMaterial, Vector3 } from 'three';
+import { BufferGeometry, EllipseCurve, Group, Vector3 } from 'three';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Line, OrbitControls } from '@react-three/drei';
 import styles from './index.less';
@@ -8,9 +8,6 @@ const Scene: React.FC = () => {
   const { camera } = useThree();
   const groupRef = useRef<Group>(null);
   const geometryRef = useRef<BufferGeometry>(null);
-  const circleGroupRef = useRef<Group>(null);
-  const figureGroupRef = useRef<Group>(null);
-  const figureGroup2Ref = useRef<Group>(null);
 
   useFrame(() => {
     groupRef.current?.children.forEach((item, index) => {
@@ -27,66 +24,6 @@ const Scene: React.FC = () => {
     const arc = new EllipseCurve(0, 0, 120, 120, 0, Math.PI * 2);
     const points = arc.getPoints(50);
     geometryRef.current?.setFromPoints(points);
-    for (let angle = 0; angle < Math.PI * 2; angle += Math.PI / 12) {
-      const R = 130;
-      const x = R * Math.cos(angle);
-      const y = R * Math.sin(angle);
-      const arc = new EllipseCurve(x, y, 10, 10, 0, Math.PI * 2);
-      const points = arc.getPoints(50);
-      const geometry = new BufferGeometry();
-      geometry.setFromPoints(points);
-      const material = new LineBasicMaterial({ color: new Color('white') });
-      const line = new ThreeLine(geometry, material);
-      circleGroupRef.current?.add(line);
-    }
-    for (let angle = 0; angle < Math.PI * 2; angle += Math.PI / 10) {
-      const R = 200;
-      const x = R * Math.cos(angle);
-      const y = R * Math.sin(angle);
-      const points = [
-        new Vector3(0, 0, 0),
-        new Vector3(10, 0, 0),
-        new Vector3(10, 10, 0),
-        new Vector3(-10, 10, 0),
-        new Vector3(-10, -10, 0),
-        new Vector3(20, -10, 0),
-        new Vector3(20, 20, 0),
-        new Vector3(-20, 20, 0),
-        new Vector3(-20, -20, 0),
-        new Vector3(20, -20, 0),
-      ];
-      const geometry = new BufferGeometry();
-      geometry.setFromPoints(points);
-      const material = new LineBasicMaterial({ color: new Color('white') });
-      const line = new ThreeLine(geometry, material);
-      line.position.set(x, y, 0);
-      line.rotation.z = angle;
-      figureGroupRef.current?.add(line);
-    }
-    for (let angle = 0; angle < Math.PI * 2; angle += Math.PI / 10) {
-      const R = 260;
-      const x = R * Math.cos(angle);
-      const y = R * Math.sin(angle);
-      const points = [
-        new Vector3(0, 0, 0),
-        new Vector3(15, 0, 0),
-        new Vector3(15, 15, 0),
-        new Vector3(-15, 15, 0),
-        new Vector3(-15, -15, 0),
-        new Vector3(30, -15, 0),
-        new Vector3(30, 30, 0),
-        new Vector3(-30, 30, 0),
-        new Vector3(-30, -30, 0),
-        new Vector3(30, -30, 0),
-      ];
-      const geometry = new BufferGeometry();
-      geometry.setFromPoints(points);
-      const material = new LineBasicMaterial({ color: new Color('white') });
-      const line = new ThreeLine(geometry, material);
-      line.position.set(x, y, 0);
-      line.rotation.z = angle;
-      figureGroup2Ref.current?.add(line);
-    }
   }, []);
 
   return (
@@ -101,14 +38,84 @@ const Scene: React.FC = () => {
           <bufferGeometry ref={geometryRef} />
           <lineBasicMaterial color='#fff' />
         </line_>
-        <group ref={circleGroupRef} />
+        <group>
+          {Array(24).fill('').map((_, index) => {
+            const angle = index * Math.PI / 12;
+            const R = 130;
+            const x = R * Math.cos(angle);
+            const y = R * Math.sin(angle);
+            const arc = new EllipseCurve(x, y, 10, 10, 0, Math.PI * 2);
+            const points = arc.getPoints(50);
+            const geometry = new BufferGeometry();
+            geometry.setFromPoints(points);
+            return (
+              <line_ key={index} geometry={geometry}>
+                <lineBasicMaterial color='#fff' />
+              </line_>
+            );
+          })}
+        </group>
         <Line
           points={new EllipseCurve(0, 0, 142.5, 142.5, 0, Math.PI * 2).getPoints(50)}
           color='#fff'
           lineWidth={5}
         />
-        <group ref={figureGroupRef} />
-        <group ref={figureGroup2Ref} />
+        <group>
+          {Array(20).fill('').map((_, index) => {
+            const angle = index * Math.PI / 10;
+            const R = 200;
+            const x = R * Math.cos(angle);
+            const y = R * Math.sin(angle);
+            const points = [
+              new Vector3(0, 0, 0),
+              new Vector3(10, 0, 0),
+              new Vector3(10, 10, 0),
+              new Vector3(-10, 10, 0),
+              new Vector3(-10, -10, 0),
+              new Vector3(20, -10, 0),
+              new Vector3(20, 20, 0),
+              new Vector3(-20, 20, 0),
+              new Vector3(-20, -20, 0),
+              new Vector3(20, -20, 0),
+            ];
+            const geometry = new BufferGeometry();
+            geometry.setFromPoints(points);
+
+            return (
+              <line_ key={index} geometry={geometry} position={[x, y, 0]} rotation={[0, 0, angle]}>
+                <lineBasicMaterial color='#fff' />
+              </line_>
+            );
+          })}
+        </group>
+        <group>
+          {Array(20).fill('').map((_, index) => {
+            const angle = index * Math.PI / 10;
+            const R = 260;
+            const x = R * Math.cos(angle);
+            const y = R * Math.sin(angle);
+            const points = [
+              new Vector3(0, 0, 0),
+              new Vector3(15, 0, 0),
+              new Vector3(15, 15, 0),
+              new Vector3(-15, 15, 0),
+              new Vector3(-15, -15, 0),
+              new Vector3(30, -15, 0),
+              new Vector3(30, 30, 0),
+              new Vector3(-30, 30, 0),
+              new Vector3(-30, -30, 0),
+              new Vector3(30, -30, 0),
+            ];
+            const geometry = new BufferGeometry();
+            geometry.setFromPoints(points);
+
+            return (
+              <line_ key={index} geometry={geometry} position={[x, y, 0]} rotation={[0, 0, angle]}>
+                <lineBasicMaterial color='#fff' />
+              </line_>
+            );
+          })}
+        </group>
       </group>
       <directionalLight args={['#fff']} position={[500, 300, 600]} />
       <ambientLight />
